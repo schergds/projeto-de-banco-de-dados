8b97d0a321d610a55d35d5944836c6c6420f0a35
schergds <schergds@gmail.com>
2025-04-28 10:51:45 -0300

    Add files via upload

---
+++ 28-04/28-04.sql
@@ -0,0 +1,30 @@
+# mostra todas as bases de dados do SGBD;
+show databases;
+
+# invoca ou chama um banco de dados específico;
+use performance_schema;
+
+# mostra as tabelas do banco de dados invocando ou em uso;
+show tables;
+
+# criar a base de dados desejada - consulta
+
+# invocar a base de dados criada
+
+# criar as tabelas
+	# para cada tabela:
+		# criar atributo com seu tipo
+		# definir restrições:
+			# atributo
+            # chave primária
+            # chave estrangeira
+            
+-- exercicio sobre a base criada...
+
+show databases;
+use mydb;
+show tables;
+
+-- na tabela médico preciso alterar o tamanho do nome... de 45 caracteres para 100 caracteres
+-- na tabela consulta preciso adicionar um atributo chamado convênio
+-- usar o alter table
